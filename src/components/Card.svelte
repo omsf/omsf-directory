88783338ea04289480ae8ae4dc3ae84494ffd559
Ethan Holz <ethan.holz@omsf.io>
2025-04-24 14:06:57 -0600

    feat: add a project tag for OMSF projects

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,31 +1,42 @@
 <script lang="ts">
-	interface Props {
-		name: string;
-		description: string;
-		docs: string;
-		license: string;
-		tags: string[];
-		link: string;
-	}
-	const { name, description, docs, license, tags, link }: Props = $props();
+  import { type SoftwareSchema } from "../content.config.ts";
+  const {
+    name,
+    description,
+    docs,
+    license,
+    tags,
+    link,
+    project,
+  }: SoftwareSchema = $props();
 </script>
 
 <div class="w-2xs overflow-hidden rounded shadow-lg lg:h-full lg:w-sm">
-	<div class="px-6 py-4">
-		<div class="font-omsf-title mb-2 text-xl font-semibold">{name}</div>
-		<div class="font-regular font-omsf-subheading mb-2 text-base">
-			{license} • <a href={docs}>Docs</a> • <a href={link}>Website</a>
-		</div>
-		<p class="font-omsf-descriptive text-base text-gray-700">
-			{description}
-		</p>
-	</div>
-	<div class="px-6 pt-4 pb-2">
-		{#each tags as tag}
-			<span
-				class="font-omsf-subheading bg-omsf-gray mr-2 mb-2 inline-block rounded-full px-3 py-1 text-sm text-gray-700"
-				>{tag}</span
-			>
-		{/each}
-	</div>
+  <div class="px-6 py-4">
+    <div class="grid grid-cols-2">
+      <div class="font-omsf-title mb-1 text-xl font-semibold">{name}</div>
+      {#if project !== undefined}
+        <div>
+          <span
+            class="font-omsf-subheading bg-omsf-gray mr-2 mb-2 inline-block rounded-full px-3 py-1 text-sm text-gray-700"
+            >{project}</span
+          >
+        </div>
+      {/if}
+    </div>
+    <div class="font-regular font-omsf-subheading font-light mb-2 text-base">
+      {license} • <a href={docs}>Docs</a> • <a href={link}>Website</a>
+    </div>
+    <p class="font-omsf-descriptive text-base text-gray-700">
+      {description}
+    </p>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    {#each tags as tag}
+      <span
+        class="font-omsf-subheading bg-omsf-gray mr-2 mb-2 inline-block rounded-full px-3 py-1 text-sm text-gray-700"
+        >{tag}</span
+      >
+    {/each}
+  </div>
 </div>
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,28 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection, z } from "astro:content";
 
-import { glob } from 'astro/loaders'
+import { glob } from "astro/loaders";
+
+const omsfProjects = z.enum([
+  "Open Force Field",
+  "Open Free Energy",
+  "Open Fold",
+]);
+
+const SoftwareSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  docs: z.string().url(),
+  license: z.string(),
+  link: z.string().url(),
+  tags: z.array(z.string()),
+  project: z.optional(omsfProjects),
+});
 
 const software = defineCollection({
-	loader: glob({ pattern: '*.yaml', base: './software' }),
-	schema: z.object({
-		name: z.string(),
-		description: z.string(),
-		docs: z.string().url(),
-		license: z.string(),
-		link: z.string().url(),
-		tags: z.array(z.string())
-	})
-})
+  loader: glob({ pattern: "*.yaml", base: "./software" }),
+  schema: SoftwareSchema,
+});
 
-export const collections = { software }
+export type SoftwareSchema = z.infer<typeof SoftwareSchema>;
+
+export const collections = { software };
