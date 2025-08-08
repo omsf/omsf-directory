24e28aa9ea4a971152029d8568a5150f42f1d5a6
Ethan Holz <ethan.holz@omsf.io>
2025-08-08 12:30:24 -0600

    fix: stringify json

--- src/pages/schema.json.ts
+++ src/pages/schema.json.ts
@@ -1,9 +1,8 @@
-import { getCollection } from 'astro:content'
 import type { APIRoute } from 'astro'
 import { jsonSchema } from '../schemas'
 
 export const GET: APIRoute = async () => {
-	return new Response(jsonSchema, {
+	return new Response(JSON.stringify(jsonSchema), {
 		status: 200,
 		headers: {
 			'Content-Type': 'application/json'
