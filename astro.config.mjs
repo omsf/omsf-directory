3d5568720cb34724d7af4cffa7557e575c8b17e7
Ethan Holz <ethan.holz@omsf.io>
2025-08-26 14:06:48 -0600

    feat: wip matomo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 
 import svelte from '@astrojs/svelte'
+import matomo from 'astro-matomo'
 
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
@@ -11,5 +12,19 @@ export default defineConfig({
 		plugins: [tailwindcss()]
 	},
 
-	integrations: [svelte()]
+	integrations: [
+		svelte(),
+		matomo({
+			enabled: import.meta.env.PROD, // Only load in production
+			// TODO: Change
+			host: 'https://analytics.example.lol/',
+			setCookieDomain: 'directory.omsf.io',
+			trackerUrl: 'js/', // defaults to matomo.php
+			srcUrl: 'js/', // defaults to matomo.js
+			siteId: 666,
+			heartBeatTimer: 5,
+			disableCookies: true,
+			debug: false
+		})
+	]
 })
